c668adb163c497686fb3a2adadee93b7d6f86768
Diego Camere <dcamere@gmail.com>
2020-01-21 01:16:08 -0500

    avance sesion2

--- src/js/script.js
+++ src/js/script.js
@@ -1,41 +1,21 @@
-var minus = document.querySelector('.resta');
-var sum = document.querySelector('.suma');
-var input = document.querySelector('.input');
-var minimum = parseInt(input.dataset.min);
-var maximum = parseInt(input.dataset.max);
+var nombre = document.getElementById("nombre");
+var botones = document.querySelectorAll(".aceptar");
+var texto;
+var valor;
 
-var isNumber = function(str) {
-	var pattern = /^\d+$/;
-	return pattern.test(str);
+var changeValue = function (e) {
+	valor = e.currentTarget.closest('.form').querySelector('input').value;
+	texto = e.currentTarget.closest('.form').querySelector('span');
+
+	if (valor!==""){
+		texto.innerHTML = valor;
+	}
+	else{
+		alert("ingrese el texto!!!!!");
+	}
 };
 
-var operar = function (e, operate) {
-	input = e.currentTarget.closest('.contador').querySelector('.input');
-	var currentValue = parseInt(input.value);
-	if (operate === 'resta' && currentValue > minimum) input.value = currentValue - 1;
-	if (operate === 'suma' && currentValue < maximum) input.value = currentValue + 1;
-};
-
-$(document).ready(function () {
-	minus.addEventListener('click', function (e) {
-		operar(e, 'resta');
-		console.log(e);
-	});
-
-	sum.addEventListener('click', function (e) {
-		operar(e, 'suma');
-	});
-
-	input.addEventListener('keyup', function (e) {
-		if (!isNumber(e.currentTarget.value.toString())) {
-			e.currentTarget.value = 0;
-		}
-
-		if (e.which == 38) {
-			operar(e, 'suma');
-		}
-		if (e.which == 40) {
-			operar(e, 'resta');
-		}
-	});
+botones.forEach(function (boton) {
+	boton.addEventListener("click", changeValue);
 });
+
